34063e3e75710f83709aa02095b9b7bc4336d4ac
xdai02 <2513276112@qq.com>
2022-02-10 17:55:24 +0800

    add chapter 3 - 6

--- code/第7章 STL标准模板库/7.4 STL字符串/string_constructor.cpp
+++ code/第7章 STL标准模板库/7.4 STL字符串/string_constructor.cpp
@@ -8,7 +8,7 @@ int main() {
     string s2(s1);			// 直接初始化，s2是s1的副本
     string s3 = s1;			// 拷贝初始化，s3是s1的副本，等价s3(s1)
     string s4("hello");		// 直接初始化，初始化为字面值常量
-    string s5 = "hello";	// 拷贝初始化，初始化为字面值常量，等价于s5("hello")
+    string s5 = "hello";	// 拷贝初始化，初始化为字面值常量
     string s6(10, 'x');		// 直接初始化，初始化为10个字符'x'
     
     cout << "s1 = " << s1 << endl;
